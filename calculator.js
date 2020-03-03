c6e36d6003dfcfe1890607b022899729c413fe45
Victor Fernandez <vmfs99@gmail.com>
2020-03-03 14:43:00 -0500

    commit

--- calculator.js
+++ calculator.js
@@ -8,6 +8,7 @@ app.get('/', (req,res) => {
     res.sendFile(__dirname + '/index.html')
 })
 
+
 app.post('/', (req,res) => {
     let num1 = Number(req.body.num1)
     let num2 = Number(req.body.num2)
